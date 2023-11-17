4cc07eab46e35081d3701f69cdb7e4e1941fc149
Victor Pérez Asuaje <v.perezasuaje@gmail.com>
2023-11-17 01:15:58 +0100

    Add project files.

---
+++ WebClient/WebClient/Program.cs
@@ -0,0 +1,32 @@
+using WebClient.Client.Components;
+using WebClient.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(NavMenu).Assembly);
+
+app.Run();
